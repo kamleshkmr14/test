22892da83b4f09c9e039da1b6a7080d9d8eb03a5
Kamlesh Kumar <kamleshkmr14@gmail.com>
2021-05-31 16:36:43 +0530

    change

--- src/component/ViewList.jsx
+++ src/component/ViewList.jsx
@@ -1,31 +1,57 @@
  import React,{useState} from "react";
+ //import Select from "react-select"
  import data from "./data.json"
- import ShowList from "./ShowList"
  import { Link, Route } from 'react-router-dom';
 
 function ViewList(){
-    let [value, setValue] = useState();
+    let [value, setValue] = useState("");
+    const ShowList = (value) => {
+        return(
+            <div className="table_container">
+            <table>
+                <thead class="tblhd">
+                    <tr>
+                        <th>NAME</th>
+                        <th>POSTAL</th>
+                        <th>STREET</th>
+                        <th>LANDMARK</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((det, ind) => {
+                        return<tr> 
+                        <td>{}</td>
+                        <td>{det.area.pin}</td>
+                        <td>{det.area.street}</td>
+                        <td>{det.area.landmark}</td>
+                    </tr>})}
+                </tbody>
+            </table>
+        </div>
+        )
+        }
+    
     return(
         <div className="main-container">
         <div className="top_header">
-            <span>View List</span>
+         <span>View List</span>
         </div>
         <div className="drop_item">
             <span>City</span>
-            <select onChange={()=>setValue()}>
+            <select onClick={(e) => setValue(e.target.value)}>
                 {data.map((detail,index)=>{
-                return <option key={index}>{detail.Name}</option>
-                
+                return <option key={index} >{detail.Name}</option> 
             })}
-            </select> <p>{value}</p>
+            </select>
+            <h4>{value}</h4>
         </div>
-
         <div className="action_btn">
             <button>Clear</button>
-            <Link to="/ShowList"> <button >RUN</button></Link>
-            <Route path="/ShowList"> <ShowList /></Route>
-        </div>
+            <button onClick={ShowList}>Run</button>
 
+           {/* <Link to="/ShowList"> <button >RUN</button></Link>
+            <Route path="/ShowList"> <ShowList /></Route>*/}
+        </div>
            </div>
     );
 }
